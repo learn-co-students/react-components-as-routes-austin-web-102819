5bf37dc74ac0382db9bb9f4e45be3bb48bedf7b7
fmwong77 <fongmeiw@gmail.com>
2020-01-16 21:34:35 -0600

    Done.

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+class About extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <h1>This is my about component!</h1>;
+  }
+}
+
+export default About;
---
+++ src/NavBar.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
+
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    /* Add basic styling for NavLinks */
+    const link = {
+      width: "100px",
+      padding: "12px",
+      margin: "0 6px 6px",
+      background: "blue",
+      textDecoration: "none",
+      color: "white"
+    };
+
+    return (
+      <div>
+        <NavLink
+          to="/"
+          /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+          exact
+          /* add styling to Navlink */
+          style={link}
+          /* add prop for activeStyle */
+          activeStyle={{
+            background: "darkblue"
+          }}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/about"
+          exact
+          style={link}
+          activeStyle={{
+            background: "darkblue"
+          }}
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/login"
+          exact
+          style={link}
+          activeStyle={{
+            background: "darkblue"
+          }}
+        >
+          Login
+        </NavLink>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
